cca8c50885bd67f413deb21cc8678ca354fba234
Saadman-Fuad <md.saadman.fuad@g.bracu.ac.bd>
2023-11-22 15:34:09 +0600

    about

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import buttonstyle from "../styles/buttons.module.css"
+
+
+
+function about() {
+    return (
+        <div>
+            about page 
+           <div className={buttonstyle.button1}>button</div>
+        </div>
+    );
+}
+
+export default about;
